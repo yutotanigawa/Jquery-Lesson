00c3db570803eb7073e13826c38566f68f455e21
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-19 18:20:06 +0900

    [Finish]6-6

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(function(){
- $('.box1').slideDown(function(){
- 	$('.box1').css({
- 		'background-color':'#0000FF',
- 		'width':'200px',
- 		'height':'100px'
- 	}).slideUp();
- });
+	$('.box1').on('click',function(){
+		$('.box1').addClass('box1-ext');
+	});
+
+	$('.box1').mouseout(function(){
+		$('.box1').removeClass('box1-ext');
+	});
 });
